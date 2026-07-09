5493451897ccc137844c23c7c3ff9797eb470fda
Volker Kerkhoff <volker@kerkhoff.es>
2026-07-09 18:32:00 +0200

    Fix delete message modal

--- bbs/cmd/bbs/confirm.go
+++ bbs/cmd/bbs/confirm.go
@@ -1,17 +1,9 @@
 package main
 
 func (a *app) confirmDelete(lang, title string) bool {
-	action, values, ok := a.runForm(lang, title, []formField{
-		{
-			name:  "confirm_delete",
-			label: a.t(lang, "confirm_delete_field"),
-			kind:  fieldChoice,
-			value: "no",
-			choices: []option{
-				{"no", a.t(lang, "no_label")},
-				{"yes", a.t(lang, "yes_label")},
-			},
-		},
-	}, []string{"no", "yes"})
-	return ok && action == "yes" && values["confirm_delete"] == "yes"
+	action := a.showInfoActions(lang, title, [][]string{{a.t(lang, "confirm_delete_field")}}, []option{
+		{"n", a.t(lang, "no_label")},
+		{"y", a.t(lang, "yes_label")},
+	})
+	return action == "y"
 }
